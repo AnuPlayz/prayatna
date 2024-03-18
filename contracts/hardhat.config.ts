46b3dbecd8ddbcf8e6ad7b6719b844846ec530d9
Aniruddh <adubge@gmail.com>
2024-03-18 23:13:42 +0530

    some contracts

---
+++ contracts/hardhat.config.ts
@@ -0,0 +1,18 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import 'dotenv/config';
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.19",
+  networks: {
+    mumbai: {
+      url: "https://polygon-mumbai.g.alchemy.com/v2/iF8yHSXHYg-40CtC1u22m45OFvIzKSh7",
+      accounts: [process.env.PRIVATE_KEY ?? ""],
+    },
+  },
+  etherscan: {
+    apiKey: "G4C7V1QEJ5GR63UJ3GMKBW1XYNPZDRTVK7"
+  }
+};
+
+export default config;
