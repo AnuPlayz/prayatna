2afefd87b77392d045011314c4c7c8e07b3a95f9
Aniruddh <adubge@gmail.com>
2024-03-19 17:10:48 +0530

    Update login button variant in site header component

--- my-app/components/site-header.tsx
+++ my-app/components/site-header.tsx
@@ -13,7 +13,7 @@ export function SiteHeader() {
         <div className="flex flex-1 items-center justify-end space-x-4">
           <nav className="flex items-center space-x-1">
             <Link href="/sign-in" className="flex items-center text-sm font-medium text-muted-foreground">
-              <Button>
+              <Button variant={"link"}>
                 Login
               </Button>
             </Link>
